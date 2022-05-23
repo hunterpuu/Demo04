4f9d6772450f7918adf37deca6c8c187d30b13ef
Hunter PUu <82997996+hunterpuu@users.noreply.github.com>
2022-05-23 22:41:44 +0700

    Update var

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
 variable "VSPHERE_USER" {
+  type        = string
+  default     = "administrator@vsphere.local"
 }
 
 variable "VSPHERE_PASS" {
+  type        = string
+  default     = "Th@1l@nd"
 }
