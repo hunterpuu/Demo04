4c0f79b52b34f050d06192b227e29e611407d631
Hunter PUu <82997996+hunterpuu@users.noreply.github.com>
2022-05-23 22:54:48 +0700

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "VSPHERE_USER" {
-  type        = string
-  default     = "administrator@vsphere.local"
-}
-
-variable "VSPHERE_PASS" {
-  type        = string
-  default     = "Th@1l@nd"
-}
